d3b8ab96199ce41186e4d8a77efa929d1a052267
Andy Dang🤖 <andy@whylabs.ai>
2021-08-02 16:10:32 -0700

    Bump version: 0.1.3 → 0.1.4-b0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,8 @@ plugins {
 }
 
 group = "ai.whylabs"
-version = "0.1.3"
-//version = "0.1.3-${project.properties.getOrDefault("versionType", "SNAPSHOT")}"
+version = "0.1.4-b0"
+//version = "0.1.4-b0-${project.properties.getOrDefault("versionType", "SNAPSHOT")}"
 extra["isReleaseVersion"] = !version.toString().endsWith("SNAPSHOT")
 
 allprojects {
