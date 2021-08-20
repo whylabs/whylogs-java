ac02143756304785e4ee4e1b62dc1d4772b59569
Andy Dang🤖 <andy@whylabs.ai>
2021-08-20 11:04:08 -0700

    Optimize for handling very wide Spark dataframes
    This allows users to avoid Spark blowing up

---
+++ spark/src/main/scala/org/apache/spark/whylogs/DatasetProfileMerger.scala
@@ -0,0 +1,34 @@
+package org.apache.spark.whylogs
+
+import com.whylogs.core.DatasetProfile
+import com.whylogs.spark.WhyLogs.PROFILE_FIELD
+import org.apache.spark.sql.catalyst.encoders.ExpressionEncoder
+import org.apache.spark.sql.expressions.Aggregator
+import org.apache.spark.sql.{Encoder, Encoders, Row}
+
+import java.io.ByteArrayInputStream
+import java.time.Instant
+import java.util.UUID
+
+class DatasetProfileMerger(datasetName: String,
+                           sessionTimeInMillis: Long,
+                           sessionId: String = UUID.randomUUID().toString) extends Aggregator[Row, DatasetProfile, Array[Byte]] {
+  override def zero: DatasetProfile =  new DatasetProfile(sessionId, Instant.ofEpochMilli(sessionTimeInMillis))
+    .withTag("Name", datasetName)
+
+  override def reduce(b: DatasetProfile, row: Row): DatasetProfile = {
+    val is = new ByteArrayInputStream(row.getAs[Array[Byte]](PROFILE_FIELD));
+    val rhs = DatasetProfile.parse(is)
+    b.merge(rhs)
+  }
+
+  override def merge(b1: DatasetProfile, b2: DatasetProfile): DatasetProfile =  {
+    b1.merge(b2)
+  }
+
+  override def finish(reduction: DatasetProfile): Array[Byte] = reduction.toBytes
+
+  override def bufferEncoder: Encoder[DatasetProfile] = Encoders.javaSerialization(classOf[DatasetProfile])
+
+  override def outputEncoder: Encoder[Array[Byte]] = ExpressionEncoder[Array[Byte]]()
+}
